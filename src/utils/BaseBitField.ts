222c389092d1ba5f247b2fdda34824c0fec3755d
OscarDev-JS <oscargamer162004@gmail.com>
2023-12-11 05:46:16 +0100

    bitfield

--- src/utils/BaseBitField.ts
+++ src/utils/BaseBitField.ts
@@ -1,9 +1,6 @@
 import { ChannelFlags } from "@discordjs/core";
-import { BitField, Permissions } from "..";
+import { BitField } from "./Bitfield";
+
 export class ChannelBitField extends BitField<ChannelFlags>{
     static Flags = ChannelFlags;
-};
-
-export class PermissionBitField extends BitField<Permissions> {
-    static Flags = Permissions;
 };
